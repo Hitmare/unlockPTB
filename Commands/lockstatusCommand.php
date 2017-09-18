209640d55101199c14a99da21aaa72ec59f54f79
Hitmare <Hitmare@users.noreply.github.com>
2017-09-18 23:54:17 +0200

    Recoding

---
+++ Commands/lockstatusCommand.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of the TelegramBot package.
+ *
+ * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Longman\TelegramBot\Commands\AdminCommands;
+namespace Hitmare\UnlockPTB;
+use Hitmare\UnlockPTB\Unlock;
+use Longman\TelegramBot\Commands\Command;
+use Longman\TelegramBot\Commands\AdminCommand;
+use Longman\TelegramBot\Request;
+
+/**
+ * User "/help" command
+ */
+class lockstatusCommand extends AdminCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'lockstatus';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Bot im Chat entsperren';
+
+    /**
+     * @var string
+     */
+    protected $usage = '/lockstatus';
+
+    /**
+     * @var string
+     */
+    protected $version = '1.1.0';
+
+    /**
+     * Command execute method
+     *
+     * @return \Longman\TelegramBot\Entities\ServerResponse
+     * @throws \Longman\TelegramBot\Exception\TelegramException
+     */
+    public function execute()
+    {
+        $message = $this->getMessage();
+        $chat_id = $message->getChat()->getId();
+		$unlock = Unlock::isUnlocked($chat_id);
+        
+       
+            $data = [
+                'chat_id' => $chat_id,
+                'text'    => $unlock,
+            ];
+
+        return Request::sendMessage($data);
+        
+		
+        
+    }
+}
---
+++ src/UnlockPTB.php
@@ -0,0 +1,118 @@
+<?php
+namespace Hitmare\UnlockPTB;
+use Longman\TelegramBot\DB;
+use PDO;
+use PDOException;
+
+class Unlock {
+    private function createRow($chat_id) {
+        try {
+            $pdo = DB::getPdo();
+            $sql = 'INSERT INTO `chat_unlock` (status, chat) VALUES (:status, :chat)';
+            $sth = $pdo->prepare($sql);
+            $sth->bindParam(':chat', $chat_id, PDO::PARAM_INT);
+            $sth->bindParam(':status', 0, PDO::PARAM_INT);
+            $sth->execute();
+            
+            return self::rowExist($chat_id);
+            
+            
+        }
+         catch (PDOException $e) {
+            throw new TelegramException($e->getMessage());
+        }
+    }
+    private function rowExist($chat_id) {
+        try {
+            $pdo = DB::getPdo();
+            $sql = 'SELECT * FROM `chat_unlock` WHERE `chat` = :chat';
+            $sth = $pdo->prepare($sql);
+            $sth->bindParam(':chat', $chat_id, PDO::PARAM_INT);
+            $sth->execute();
+            $row = $sth->fetch(PDO::FETCH_ASSOC);
+            if(!$row){
+                return FALSE;
+            }
+            elseif($row){
+                return TRUE:
+            }
+        }
+         catch (PDOException $e) {
+            throw new TelegramException($e->getMessage());
+        }
+        
+    }
+    public function isUnlocked($chat_id) {
+        if(!self::rowExist($chat_id)){
+            if(!self::createRow($chat_id)) {
+                return 'ERROR';
+            }
+        }
+        try {
+            
+            $pdo = DB::getPdo();
+            $sql = 'SELECT `status` FROM `chat_unlock` WHERE `chat` = :chat';
+            $sth = $pdo->prepare($sql);
+            $sth->bindParam(':chat', $chat_id, PDO::PARAM_INT);
+            $sth->execute();
+            $row = $sth->fetch();
+
+            return boolval($row['status']);
+            
+        }
+         catch (PDOException $e) {
+            throw new TelegramException($e->getMessage());
+        }
+    }
+    
+    public function unlockChannel($chat_id) {
+                   
+        
+        try {
+            $pdo = DB::getPdo();
+            if(!self:rowExist($chat_id){
+                $sql = 'INSERT INTO `chat_unlock` (status, chat) VALUES (:status, :chat)';
+            }
+            else {
+               $sql = 'UPDATE `chat_unlock` SET `status` = :status WHERE `chat` = :chat'; 
+            }
+            $sth = $pdo->prepare($sql);
+            $sth->bindParam(':chat', $chat_id, PDO::PARAM_INT);
+            $sth->bindParam(':status', 1, PDO::PARAM_INT);
+            $sth->execute();
+            
+            return self::isUnlocked($chat_id);
+            
+            
+        }
+         catch (PDOException $e) {
+            throw new TelegramException($e->getMessage());
+        }
+    }
+    public function lockChannel($chat_id) {
+        
+        try {
+            $pdo = DB::getPdo();
+            $sql = 'UPDATE `chat_unlock` SET `status` = :status WHERE `chat` = :chat';
+            $sth = $pdo->prepare($sql);
+            $sth->bindParam(':chat', $chat_id, PDO::PARAM_INT);
+            $sth->bindParam(':status', 0, PDO::PARAM_INT);
+            $sth->execute();
+            
+            return self::isUnlocked($chat_id);
+            
+            
+        }
+         catch (PDOException $e) {
+            throw new TelegramException($e->getMessage());
+        }
+    }
+    
+    
+    
+    
+    
+    
+}
+
+?>
