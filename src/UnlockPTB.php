eb14afadb61df74da34382ddfcbbbc68bef6c859
Hitmare <Hitmare@users.noreply.github.com>
2017-09-19 15:54:47 +0200

    Some Fixes

--- src/UnlockPTB.php
+++ src/UnlockPTB.php
@@ -1,20 +1,27 @@
 <?php
 namespace Hitmare\UnlockPTB;
 use Longman\TelegramBot\DB;
+use Longman\TelegramBot\Exception\TelegramException;
 use PDO;
 use PDOException;
 
 class Unlock {
-    private function createRow($chat_id) {
+    /**
+     * @param $chat_id1
+     * @return bool
+     * @throws TelegramException
+     */
+    private function createRow($chat_id1) {
         try {
+            $val = 0;
             $pdo = DB::getPdo();
             $sql = 'INSERT INTO `chat_unlock` (status, chat) VALUES (:status, :chat)';
             $sth = $pdo->prepare($sql);
-            $sth->bindParam(':chat', $chat_id, PDO::PARAM_INT);
-            $sth->bindParam(':status', 0, PDO::PARAM_INT);
+            $sth->bindParam(':chat', $chat_id1, PDO::PARAM_INT);
+            $sth->bindParam(':status', $val, PDO::PARAM_INT);
             $sth->execute();
             
-            return self::rowExist($chat_id);
+            return self::rowExist($chat_id1);
             
             
         }
@@ -22,29 +29,42 @@ class Unlock {
             throw new TelegramException($e->getMessage());
         }
     }
-    private function rowExist($chat_id) {
+
+    /**
+     * @param $chat_id2
+     * @return bool
+     * @throws TelegramException
+     */
+    private function rowExist($chat_id2) {
         try {
+            $val = '';
             $pdo = DB::getPdo();
             $sql = 'SELECT * FROM `chat_unlock` WHERE `chat` = :chat';
             $sth = $pdo->prepare($sql);
-            $sth->bindParam(':chat', $chat_id, PDO::PARAM_INT);
+            $sth->bindParam(':chat', $chat_id2, PDO::PARAM_INT);
             $sth->execute();
             $row = $sth->fetch(PDO::FETCH_ASSOC);
-            if(!$row){
-                return FALSE;
+                if(!$row){
+                    $val = FALSE;
+                }
+                elseif($row){
+                    $val = TRUE;
+                }
+                return $val;
             }
-            elseif($row){
-                return TRUE;
-            }
-        }
-         catch (PDOException $e) {
+        catch (PDOException $e) {
             throw new TelegramException($e->getMessage());
         }
         
     }
-    public function isUnlocked($chat_id) {
-        if(!self::rowExist($chat_id)){
-            if(!self::createRow($chat_id)) {
+
+    /**
+     * @param $chat_id3
+     * @return bool|string
+     */
+    public static function isUnlocked($chat_id3) {
+        if(!self::rowExist($chat_id3)){
+            if(!self::createRow($chat_id3)) {
                 return 'ERROR';
             }
         }
@@ -64,24 +84,29 @@ class Unlock {
             throw new TelegramException($e->getMessage());
         }
     }
-    
-    public function unlockChannel($chat_id) {
+
+    /**
+     * @param $chat_id4
+     * @return bool|string
+     */
+    public static function unlockChannel($chat_id4) {
                    
         
         try {
+            $val = 1;
             $pdo = DB::getPdo();
-            if(!self:rowExist($chat_id){
+            if(!self::rowExist($chat_id4){
                 $sql = 'INSERT INTO `chat_unlock` (status, chat) VALUES (:status, :chat)';
             }
             else {
                $sql = 'UPDATE `chat_unlock` SET `status` = :status WHERE `chat` = :chat'; 
             }
             $sth = $pdo->prepare($sql);
-            $sth->bindParam(':chat', $chat_id, PDO::PARAM_INT);
-            $sth->bindParam(':status', 1, PDO::PARAM_INT);
+            $sth->bindParam(':chat', $chat_id4, PDO::PARAM_INT);
+            $sth->bindParam(':status', $val, PDO::PARAM_INT);
             $sth->execute();
             
-            return self::isUnlocked($chat_id);
+            return self::isUnlocked($chat_id4);
             
             
         }
@@ -89,17 +114,23 @@ class Unlock {
             throw new TelegramException($e->getMessage());
         }
     }
-    public function lockChannel($chat_id) {
+
+    /**
+     * @param $chat_id5
+     * @return bool|string
+     */
+    public static function lockChannel($chat_id5) {
         
         try {
+            $val = 0;
             $pdo = DB::getPdo();
             $sql = 'UPDATE `chat_unlock` SET `status` = :status WHERE `chat` = :chat';
             $sth = $pdo->prepare($sql);
-            $sth->bindParam(':chat', $chat_id, PDO::PARAM_INT);
-            $sth->bindParam(':status', 0, PDO::PARAM_INT);
+            $sth->bindParam(':chat', $chat_id5, PDO::PARAM_INT);
+            $sth->bindParam(':status', $val, PDO::PARAM_INT);
             $sth->execute();
             
-            return self::isUnlocked($chat_id);
+            return self::isUnlocked($chat_id5);
             
             
         }
